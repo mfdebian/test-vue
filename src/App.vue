39496cfd6c16e508a5ae458e7379e581b5fd8d75
daydream <mfdebian@github.com>
2019-06-12 20:41:47 -0400

    Component data binding and v-bind v-for directives

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,48 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Hello world"/>
     <TestComponent msg="welcome to the lain"/>
+    <!-- bind postsArray to Posts component as a prop -->
+    <Posts v-bind:postsArray="postsArray" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import TestComponent from './components/TestComponent.vue'
+import Posts from './components/Posts.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    TestComponent
+    TestComponent,
+    Posts
+  },
+  // data() is a vue native function _apparently_
+  data() {
+    return {
+      postsArray: [
+        {
+          id: 0,
+          title: 'math',
+          body: 'exponential functions',
+          tags: ['math', 'functions', 'exponential']
+        },
+        {
+          id: 1,
+          title: 'comunications',
+          body: 'political facts',
+          tags: ['comunications', 'facts', 'politics']
+
+        },
+        {
+          id: 2,
+          title: 'music',
+          body: 'flac download',
+          tags: ['music', 'flac', 'download']
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Posts.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- iteration through postsArray array -->
+    <!-- iteration requires to bind a unique key -->
+    <div v-bind:key="post.id" v-for="post in postsArray">
+      <h2>{{ post.title }}</h2>
+      <h3>{{ post.body }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Posts",
+    /* assing postsArray import into props
+      'props' is a native property of the component */
+    props: ["postsArray"]
+  }
+</script>
+
+<style scoped>
+
+</style>
